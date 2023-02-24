4d4fbf1cfb0f0acf502bf318308843b0c3d67147
ron <yrong1997@gmail.com>
2023-02-25 02:46:55 +0800

    Use optional param instead

--- rpc/mmr/generate_proof.go
+++ rpc/mmr/generate_proof.go
@@ -19,7 +19,7 @@ func (c *MMR) GenerateProofLatest(blockNumber uint32) (types.GenerateMMRProofRes
 func (c *MMR) generateProof(blockNumber uint32, blockHash *types.Hash) (types.GenerateMMRProofResponse, error) {
 	var res types.GenerateMMRProofResponse
 	blocks := [1]uint32{blockNumber}
-	err := client.CallWithBlockHash(c.client, &res, "mmr_generateProof", blockHash, blocks, blockNumber)
+	err := client.CallWithBlockHash(c.client, &res, "mmr_generateProof", blockHash, blocks, nil)
 	if err != nil {
 		return types.GenerateMMRProofResponse{}, err
 	}
